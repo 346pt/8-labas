25944209c757ff7fefd5c09c0c431680b366053a
The4yo <eugshpak@yandex.ru>
2023-04-11 22:21:19 +0300

    aurora1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using ConsoleApp1;
-
+// int a
 TS auto = new Auto { brand = "BMW", colour = "Blue" };
 TS trail = new Train { colour = "Red", vagoni = "Десять" };
 TS plane = new Plane { colour = "White", mesta = "Сто" };
